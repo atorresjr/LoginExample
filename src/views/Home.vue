dfbd73c6c42c5b5f358f320f578f5807c5d0c4d2
Alfonso Torres <atorresjr1@hotmail.com>
2021-03-14 23:01:04 -0500

    Added login email/password

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,1 +1,10 @@
-<template>Home Page.</template>
+<template>
+<div class="mt-80">
+<p class="text-3xl mx-auto text-center m-8 tracking-wider font-thin"> Welcome {{ user?.email || 'New User' }} </p>
+    <img class="h-60 mx-auto mt-8" src="../assets/home.png" alt="Home Image" />
+</div>
+</template>
+
+<script setup>
+import {user} from '../helpers/useAuth'
+</script>
